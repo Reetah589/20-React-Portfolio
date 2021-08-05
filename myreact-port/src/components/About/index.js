47f1032c037462ad8682ddf22f2c8cdb616d6b80
reetah589 <reetah589@gmail.com>
2021-08-05 17:38:12 -0500

    edited app.js and created file components

---
+++ myreact-port/src/components/About/index.js
@@ -0,0 +1,12 @@
+import { func } from 'prop-types';
+import React from 'react';
+
+function About() {
+    return (
+        <section>
+            <h1 id="about">My Biography</h1>
+        </section>
+    );
+}
+
+export default About;
