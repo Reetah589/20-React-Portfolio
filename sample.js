877c793bd3507f8bc7fa46080bbb5dfc730eab49
reetah589 <reetah589@gmail.com>
2021-08-05 19:52:01 -0500

    components nav folder added

--- sample.js
+++ sample.js
@@ -1,5 +1,8 @@
 // testing the first commit 
 
+// There was an issue with the browser, I was getting the classroom activity "temperature" browser instead of what I was creating. AFter getting help from AskBCS 
+    //we managed to not see the browser anymore bu then other node issues prevent from seeing the browser. below are suggestions of what to do: 
 // Try deleting the whole folder - and recloning down the folder again from github, then running npm i (in same level as package.json) and running npm start
-
 // recloning and reinstalling fresh basically
+
+// I am going to keep coding and ask TA's for help during office hours.
