f303f499344eea4870bc81e975ad5b039a1c6c49
reetah589 <reetah589@gmail.com>
2021-08-05 19:17:12 -0500

    major node issues and left notes on sample.js on what to do next

--- sample.js
+++ sample.js
@@ -1,1 +1,5 @@
 // testing the first commit 
+
+// Try deleting the whole folder - and recloning down the folder again from github, then running npm i (in same level as package.json) and running npm start
+
+// recloning and reinstalling fresh basically
